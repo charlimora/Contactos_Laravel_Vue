6cc611405532bdea69b3eb52ae0f9c537ec6144b
charlimora <ca.agudelo.2021@gmail.com>
2022-08-26 22:10:12 -0500

    Se dio funcionalidad para traer todos los contactos al compontente Vue

--- Contactos_Laravel_Vue/resources/js/app.js
+++ Contactos_Laravel_Vue/resources/js/app.js
@@ -15,8 +15,8 @@ import { createApp } from 'vue';
 
 const app = createApp({});
 
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('myexample', ExampleComponent);
+import Contact from './components/Contact.vue';
+app.component('contact', Contact);
 
 /**
  * The following block of code may be used to automatically register your
---
+++ Contactos_Laravel_Vue/resources/js/components/Contact.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h1 class="text-center">Agenda de Contactos</h1>
+        <hr>
+        <table class="table">
+            <thead class="table-dark">
+                <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Email</th>
+                <th scope="col">Teléfono</th>
+                <th scope="col">Dirección</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="contacto in contacts" :key="contacto.id">
+                <th scope="row">{{ contacto.id }}</th>
+                <td>{{ contacto.nombre }}</td>
+                <td>{{ contacto.apellido }}</td>
+                <td>{{ contacto.email }}</td>
+                <td>{{ contacto.tele }}</td>
+                <td>{{ contacto.direccion }}</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                contacts: [],
+            }
+        },
+        methods: {
+            async listado() {
+                const res = await axios.get('contactos')
+                this.contacts = res.data
+            }
+        },
+        created() {
+            this.listado()
+        },
+    }
+</script>
+
+<style>
+
+</style>
